fbaa1b23e0ccc2b843a5d7e9fafc136085e6243d
@jarek564as <jarek564as@gamil.com>
2020-10-14 19:03:11 +0200

    pierwszy model i build

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using PortalApi.Models;
 
 namespace PortalApi.Data
 {
     public class DataContext:DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options): base (options){}
+        public DbSet<Value> Values { get; set; }
     }
 }
